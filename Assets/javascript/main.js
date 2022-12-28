1c0554b76b436d0d8fc80a2a4277dcd21a58b5f9
Satya Prakash <satyaprakash01.gh@gmail.com>
2022-12-28 18:47:03 +0530

    fifth day commit

--- Assets/javascript/main.js
+++ Assets/javascript/main.js
@@ -32,3 +32,46 @@ $('.owl-bestseller').owlCarousel({
     // autoplayHoverPause:true,
     dots:false,
 });
+const accordion = document.querySelectorAll(".custom-menu");
+
+accordion.forEach((el) =>
+  el.addEventListener("click", () => {
+    let childs = el.children;
+    
+    if (childs[0].classList.contains("active")) {
+        childs[0].classList.remove("active");
+        childs[1].classList.remove("active");
+    } else {
+        childs[0].classList.add("active");
+        childs[1].classList.add("active");
+      accordion.forEach((el2) => el2.classList.remove("active"));
+      childs[0].classList.add("active");
+    }
+  })
+);
+const menubtn = document.querySelector('.toggler');
+const closebtn = document.querySelector('.close');
+const mobile_menu = document.querySelector('.mobile-menu');
+let menuopen = false;
+menubtn.addEventListener('click', () =>{
+    if(!menuopen){
+        menubtn.classList.add('toggler-dis');
+        mobile_menu.classList.add('active-menu');
+        closebtn.classList.add('active-menu');
+        menuopen = true;
+    }
+    // else{
+    //     closebtn.classList.remove('active-menu');
+    //     mobile_menu.classList.remove('active-menu');
+    //     menuopen = false;
+    // }
+})
+closebtn.addEventListener('click', ()=>{
+    if(menuopen){
+        menubtn.classList.remove('toggler-dis')
+        closebtn.classList.remove('active-menu');
+        mobile_menu.classList.remove('active-menu');
+        menuopen = false;
+        
+    }
+})
